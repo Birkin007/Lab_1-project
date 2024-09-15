b03d956568be2d1af4be3c99254ca0eb7ed04060
konst <konstantin.birmenko@gmail.com>
2024-09-15 23:01:35 +0300

    time_of_day

---
+++ time_of_day/hello.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    time_t rawtime;
+    struct tm * timeinfo;
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    int hour = timeinfo->tm_hour;
+
+    if (hour >= 5 && hour < 12)
+        printf("Good morning!\n");
+    else if (hour >= 12 && hour < 17)
+        printf("Good day!\n");
+    else if (hour >= 17 && hour < 23)
+        printf("Good night!\n");
+    else
+        printf("It's too late, let's go to bed.\n");
+
+    return 0;
+}
